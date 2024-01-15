15f2f1cad78613efa7d80c92b64c7907221939bb
AlexanderDubelschikov <109314029+AlexanderDubelschikov@users.noreply.github.com>
2024-01-15 03:18:58 +0300

    Next Step

---
+++ Android_Lesson/App2/app/src/main/java/sokragud/example/app2/ItemActivity.kt
@@ -0,0 +1,24 @@
+package sokragud.example.app2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class ItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item)
+
+        val title: TextView = findViewById(R.id.item_list_title_one)
+        val text: TextView = findViewById(R.id.item_list_text_one)
+        val price: TextView = findViewById(R.id.item_list_price_one)
+
+
+        title.text = intent.getStringExtra("itemTitle")
+        text.text = intent.getStringExtra("itemText")
+        price.text = intent.getStringExtra("itemPrice")
+
+    }
+}
--- Android_Lesson/App2/app/src/main/java/sokragud/example/app2/ItemsAdapter.kt
+++ Android_Lesson/App2/app/src/main/java/sokragud/example/app2/ItemsAdapter.kt
@@ -1,9 +1,11 @@
 package sokragud.example.app2
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -16,6 +18,7 @@ class ItemsAdapter(var items: List<Item>, var context: Context): RecyclerView.Ad
         val title: TextView = view.findViewById(R.id.item_list_title)
         val desc: TextView = view.findViewById(R.id.item_list_desc)
         val price: TextView = view.findViewById(R.id.item_list_price)
+        val btn: Button = view.findViewById(R.id.item_list_button)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -39,6 +42,16 @@ class ItemsAdapter(var items: List<Item>, var context: Context): RecyclerView.Ad
         )
 
         holder.image.setImageResource(imageId)
+
+        holder.btn.setOnClickListener{
+            val intent = Intent(context, ItemActivity::class.java)
+
+            intent.putExtra("itemTitle", items[position].title)
+            intent.putExtra("itemText", items[position].text)
+            intent.putExtra("itemPrice", items[position].price)
+
+            context.startActivity(intent)
+        }
     }
 }
 
