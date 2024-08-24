3accab5987ec32c370bcc2e596bc0cb516088974
AlexanderDubelschikov <109314029+AlexanderDubelschikov@users.noreply.github.com>
2024-08-24 16:25:47 +0300

    Lesson_6

--- Android_Studio_Lessons/2LayoutView/app/src/main/java/sokragud/example/a2layoutview/MainActivity.kt
+++ Android_Studio_Lessons/2LayoutView/app/src/main/java/sokragud/example/a2layoutview/MainActivity.kt
@@ -7,12 +7,15 @@ import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    val const = 10
+    var context = "Hello World"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //R.id.MainButton
         //R.id.MainTextView
+        var counter = 10
 
         }
 
